4af63f0f6c1064e9c4a2902285cee25b24cd326a
Said Salas <saidsalastech@gmail.com>
2025-10-27 11:06:37 -0600

    starbucks data done

---
+++ data-transformations/starbucks.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+coffee = pd.read_csv('/Users/cristosaidsalaslandaverde/Documents/machine-learning/data-transformations/starbucks_customers.csv')
+print(coffee)
