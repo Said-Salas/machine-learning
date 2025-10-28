b0aeef14e712fe655ad7083f0d45760ae3299a2f
Said Salas <saidsalastech@gmail.com>
2025-10-27 18:55:04 -0600

    numerical transformations on starbucks data

--- data-transformations/starbucks.py
+++ data-transformations/starbucks.py
@@ -3,4 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 coffee = pd.read_csv('/Users/cristosaidsalaslandaverde/Documents/machine-learning/data-transformations/starbucks_customers.csv')
-print(coffee)
+ages = coffee['age']
+mean_age = np.mean(ages)
+std_dev_age = np.std(ages)
+ages_standarized = (ages - mean_age) / std_dev_age
+print(ages_standarized)
